7dfa2cf016df880853f3cdb1e6525dc50953a6cb
Gabriela Nagamuta <gabrielanagamuta@timepix.com.br>
2025-05-14 17:58:52 -0300

    adiciona contexto para utilizar dados em diferentes páginas

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import Header from "./Components/Header"
 import Sidenav from "./Components/Sidenav"
 import Resumo from "./Pages/Resumo"
 import "./Style.css"
+import { DataContextProvider } from "./Context/DataContext"
 
 function App() {
 
   return (
-    <div>
-      <Sidenav />
-      <main>
-        <Resumo />
-        <Header />
-      </main>
-    </div>
+    <DataContextProvider>
+      <div>
+        <Sidenav />
+        <main>
+          <Resumo />
+          <Header />
+        </main>
+      </div>
+    </DataContextProvider>
   )
 }
 
