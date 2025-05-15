25dc24e7f07f21f557009e81ab42c9d8c657f315
Gabriela Nagamuta <gabrielanagamuta@timepix.com.br>
2025-05-15 20:15:10 -0300

    adiciona o componente de data

---
+++ src/Components/DateInput.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type IDateInput = React.ComponentProps<"input"> & {
+    label: string;
+};
+
+const DateInput = ({label, ...props}: IDateInput) => {
+  return (
+    <div>
+        <label htmlFor={label}>{label}</label>
+        <input type="date" id={label} name={label} {...props}/>
+    </div>
+  );
+};
+
+export default DateInput
